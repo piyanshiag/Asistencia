7fb311c9bbb9cc4638ed5d808922e623638e34c7
piyanshi10 <piyanshi1091@gmail.com>
2022-05-29 14:47:19 +0530

    envremove

--- automatic_attendance/automatic_attendance/settings.py
+++ automatic_attendance/automatic_attendance/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-7j=@ua%qvih_!18nd^www3%8*xc6@@#ntvx_=(+fm(9!+n7(*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
